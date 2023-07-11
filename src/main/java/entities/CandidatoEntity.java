ae852a27f776fd2029b786a55ab99463337be260
Beatriz <beatriz.mediatto2@gmail.com>
2023-07-11 09:12:53 -0300

    feat: add entidade Candidato

---
+++ src/main/java/entities/CandidatoEntity.java
@@ -0,0 +1,25 @@
+package entities;
+
+public class CandidatoEntity {
+	private String nome;
+	private String status;
+	
+	public CandidatoEntity(String nome, String status) {
+		this.nome = nome;
+		this.status = status;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+}
